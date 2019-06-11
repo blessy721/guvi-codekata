a8a274bf01419e6ff99e0fe53fe03c637171b496
Hemanth Mudra <47498538+hmswrth@users.noreply.github.com>
2019-06-11 15:40:52 +0530

    unique numbers in sorted order

---
+++ hunter_1.py
@@ -0,0 +1,17 @@
+def unique(arr,count):
+    stack=[]
+    for i in range(0,count):
+        for j in range(i+1,count):
+            if arr[i]==arr[j]:
+                if arr[i] not in stack:
+                    stack.append(arr[i])
+    if (len(stack)>0):
+        for i in range(len(stack)):
+            print(stack[i],end=' ')
+    else:
+        print("Unique")
+
+size=int(input())
+arr=list(map(int,input()))
+count=len(arr)
+unique(arr,count)
