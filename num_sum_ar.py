41b17d432702dfadb7ca00d80a61cb30f009fd72
hmswrth <mudra.hemanth@gmail.com>
2019-06-18 13:14:53 +0530

    if the sum of any two numbers in an array equals give sum

---
+++ num_sum_ar.py
@@ -0,0 +1,10 @@
+a=list(map(int,input().split()))
+count=a[0]
+s=a[1]
+b=list(map(int,input().split()))
+for i in range(len(b)):
+    for j in range(i+1,len(b)):
+        if b[i]+b[j]==s:
+            print('yes')
+        else:
+            print('no')
