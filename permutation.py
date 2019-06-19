8de51f26b52bc4741fe0d6cbfcc50dea62442103
hmswrth <mudra.hemanth@gmail.com>
2019-06-20 03:16:33 +0530

    permutations of number

--- permutation.py
+++ permutation.py
@@ -6,10 +6,10 @@ def permm(n):
         return [n]
     for i in range(len(n)):
         a=n[i]
-        rem = n[:i] + n[i+1:]
+        rem = n[:i]+n[i+1:]
         for p in permm(rem):
             lis.append([a]+p)
     return lis
-n=list(map(int,input()))
-for p in permm(n):
-    print(*p)
+n=list(input())
+for i in permm(n):
+    print("".join(i))
