3cc445154c34812e4889884081e8d6dda9531710
hmswrth <mudra.hemanth@gmail.com>
2019-06-20 01:32:09 +0530

    i+j=k

---
+++ equa.py
@@ -0,0 +1,9 @@
+def portt(n,arr):
+    for i in range(0,n-2):
+        for j in range(i+1,n-1):
+            for k in range(j+1,n):
+                if arr[i]+arr[j] == arr[k]:
+                    print(arr[i], arr[j], arr[k])
+n=int(input())
+arr=list(map(int,input().split()))
+portt(n,arr)
