f333c71d7e58c873b223c5c042781eab376ec6d0
hmswrth <mudra.hemanth@gmail.com>
2019-06-20 01:49:17 +0530

    sum of two nums is close or equal to 0

--- sum_zero.py
+++ sum_zero.py
@@ -2,8 +2,9 @@ n=int(input())
 arr=list(map(int,input().split()))
 c=len(arr)
 large=max(arr)
+y,z=0,0
 for i in range(0,c-1):
-    for j in range(0,c):
+    for j in range(i+1,c):
         if abs(arr[i]+arr[j])< large:
             y,z=arr[i],arr[j]
             large=abs(y+z)
