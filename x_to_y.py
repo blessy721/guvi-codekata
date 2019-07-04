2833ea723592bf69dc8adf4d2b2f7f289ce12794
hmswrth <mudra.hemanth@gmail.com>
2019-07-05 01:08:51 +0530

    x string to y at least cost

--- x_to_y.py
+++ x_to_y.py
@@ -1,10 +1,10 @@
-sa,sb=input().split()
-ans=abs(len(sa)-len(sb))
-def x_to_y(ans,sa,sb):
-    for i in range(len(sa)):
-        if len(sb)==1  and sb[i]==sa[i]:
+st1,st2=input().split()
+answ=abs(len(st1)-len(st2))
+def x_to_y(ans,s1,s2):
+    for i in range(len(s1)):
+        if len(s2)==1  and s2[i]==s1[i]:
             break
-        if sa[i]!=sb[i]:
+        if s1[i]!=s2[i]:
             ans+=1
     return ans
-print(x_to_y(ans,sa,sb))
+print(x_to_y(answ,st1,st2))
