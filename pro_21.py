504e8edf14a2373471fcf721fb36b153582ec77b
hmswrth <mudra.hemanth@gmail.com>
2019-07-09 00:23:13 +0530

    pro 21

--- pro_21.py
+++ pro_21.py
@@ -1,13 +1,13 @@
 import statistics as st
 count=int(input())
 arr=list(map(int,input().split()))
-yes=False
+res=False
 for i in range(1,count):
     l1=arr[:i]
     l2=arr[i:]
     if st.mean(l1)==st.mean(l2):
-        yes=True
-if yes==True:
+        res=True
+if res==True:
     print("yes")
 else:
     print("no")
