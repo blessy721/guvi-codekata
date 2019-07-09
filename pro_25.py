fb18e5faab7ec87ce6ed7b5074cddc96d521cadc
hmswrth <mudra.hemanth@gmail.com>
2019-07-10 00:46:34 +0530

    pro 25

--- pro_25.py
+++ pro_25.py
@@ -1,13 +1,14 @@
 def LIS(arr,size):
     res=[]
     count=1
-    for i in arr:
-        if i not in res:
-            res.append(i)
-    for i in range(len(res)-1):
-        if res[i]<res[i+1]:
+    for i in range(0,size-1):
+        if arr[i]<arr[i+1]:
             count+=1
-    print(count)
+        else:
+            res.append(count)
+            count=1
+    res.append(count)
+    print(max(res))
 size=int(input())
 arr=list(map(int,input().split()))
 LIS(arr,size)
